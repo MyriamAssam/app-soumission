9bf32061e2fa54f8ae8552078fed87ea7ca00212
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-03-25 16:45:06 -0400

    ajuster backend soumissions

--- Backend/models/soumission.js
+++ Backend/models/soumission.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const soumissionSchema = new mongoose.Schema({
     adresse: { type: String, required: false },
     nomEmployeur: { type: String },
-    prenomClient: { type: String, required: true },
+    prenomClient: { type: String, required: false },
     email: { type: String, required: true },
     description: { type: String, required: false },
-    telephone: { type: String, required: true },
+    telephone: { type: String, required: false },
     employeurId: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
     notes: { type: String, default: "" },
     travaux: [{ type: String }]

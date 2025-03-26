9ceaea512dfa5fb326a16f49edf7ab816789ea98
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-03-26 11:00:00 -0400

    arranger soumissions

--- Backend/app.js
+++ Backend/app.js
@@ -1,44 +1,42 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const usersRoutes = require("./routes/users-routes");
 const soumisRoutes = require("./routes/soumis-routes");
-
 const errorHandler = require("./handler/error-handler");
 
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
-});
-app.options("*", (req, res) => {
-  res.sendStatus(200);
-});
+// CORS autorisé uniquement pour ton frontend déployé
+app.use(cors({
+  origin: "https://app-soumission.onrender.com",
+  credentials: true
+}));
 
-// routes
+app.options("*", (req, res) => res.sendStatus(200));
+
+// Routes
 app.use("/soumis", soumisRoutes);
 app.use("/soumissions", soumisRoutes);
 app.use("/users", usersRoutes);
 
+// Gestion des routes non trouvées
 app.use((req, res, next) => {
   const error = new Error("Route non trouvée");
   error.code = 404;
   next(error);
 });
+
+// Gestion des erreurs
 app.use(errorHandler);
 
+// Connexion MongoDB
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGO_URI;
 
-mongoose
-  .connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI)
   .then(() => {
     app.listen(PORT);
     console.log(`Connexion à la BD [${MONGODB_URI}] sur le port ${PORT} réussie.`);

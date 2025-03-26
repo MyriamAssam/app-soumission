2b371da591fb8057b34d8f76b8cb49f3bf667764
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-03-26 12:52:59 -0400

    arranger css

---
+++ Frontend/src/containers/Connexion.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import Login from "../components/login/Login";
+
+import axios from "axios";
+
+export default function Connexion() {
+  const [typeCompte, setTypeCompte] = useState("Client");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [offers, setOffers] = useState([]);
+  const [error, setError] = useState(null);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  useEffect(() => {
+    const fetchOffers = async () => {
+      if (isAuthenticated) {
+        try {
+          let response;
+          if (typeCompte === "Client") {
+            response = await axios.get(
+              process.env.REACT_APP_BACKEND_URL + "soumissions/"
+            );
+          } else if (typeCompte === "Employé") {
+            const employeurId = "66f1d6d0dbe127215621f263";
+            response = await axios.get(
+              process.env.REACT_APP_BACKEND_URL + `offers-Entrp/${employeurId}/`
+            );
+          }
+          setOffers(response.data.offres);
+        } catch (err) {
+          setError("Erreur lors de la récupération des soumissions.");
+        }
+      }
+    };
+
+    fetchOffers();
+  }, [isAuthenticated, typeCompte]);
+
+  return (
+    <div>
+      <Login type={typeCompte} onLogin={handleLogin} />
+      <div className="typeCompte">
+
+        <a onClick={() => setTypeCompte("Client")}>Client</a>
+        <a onClick={() => setTypeCompte("Employé")}>Employé</a>
+      </div>
+
+
+
+      {error && <div>{error}</div>}
+
+
+    </div>
+  );
+}

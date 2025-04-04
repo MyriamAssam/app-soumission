58ffd1e99f4711e25e5f34d4c9c0f50496b27a6d
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-04-04 14:24:22 -0400

    Ajustement informations soumission

--- Frontend/src/components/login/Login.jsx
+++ Frontend/src/components/login/Login.jsx
@@ -35,13 +35,13 @@ export default function Login(props) {
       const responseData = await response.json();
       console.log("1", responseData);
       auth.login(
-        responseData.user.id,
+        responseData.userId,
         responseData.token,
-        responseData.user.prenom,
-        responseData.user.email,
-        responseData.user.adresse,
-        responseData.user.telephone,
-        responseData.user.role
+        responseData.prenom,
+        responseData.email,
+        responseData.adresse,
+        responseData.telephone,
+        responseData.role.toLowerCase()
       );
 
 

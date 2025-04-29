6c7a0904c73190015eb779360f993ab2d9672c6c
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-04-29 17:30:34 -0400

    modification de profil

--- Frontend/src/containers/Profile.jsx
+++ Frontend/src/containers/Profile.jsx
@@ -54,7 +54,8 @@ export default function Profile() {
         if (!user) return;
 
         try {
-            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}users/${user.userId}`, {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}users/${user.id}`, {
+
 
                 method: "PATCH",
                 headers: {

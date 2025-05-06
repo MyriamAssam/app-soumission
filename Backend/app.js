3010a4dfecaa9e79e805ae3cda404f9b82569843
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-05-06 14:20:08 -0400

    sdsd

--- Backend/app.js
+++ Backend/app.js
@@ -16,23 +16,31 @@ app.use(cors({
   credentials: true
 }));
 
+// Ajout recommandé pour les requêtes préflight
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://app-soumission.onrender.com");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.use(bodyParser.json());
 app.options("*", (req, res) => res.sendStatus(200));
 
-
+// Routes
 app.use("/soumis", soumisRoutes);
 app.use("/soumissions", soumisRoutes);
 app.use("/users", usersRoutes);
 
-
+// 404 handler
 app.use((req, res, next) => {
   const error = new Error("Route non trouvée");
   error.code = 404;
   next(error);
 });
 
-
 app.use(errorHandler);
+
 
 
 const PORT = process.env.PORT || 3000;

a76de0c0b1465593b510f405f4853a47c1a1010b
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-05-08 11:54:33 -0400

    langues

--- Frontend/src/components/App.js
+++ Frontend/src/components/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
-
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import "./App.css";
 import Register from "../containers/Register";
 import Connexion from "../containers/Connexion";
 import AddSoumi from "../containers/AddSoumi";
@@ -13,6 +9,7 @@ import SoumissionsList from "./SoumissionsList/SoumissionsList";
 import DetailSoumission from "./DetailSoumission/DetailSoumission";
 import AllSoumissions from "../containers/AllSoumissions";
 import Profile from "../containers/Profile";
+import { AuthProvider } from "./context/AuthContext"; // 👈 IMPORTANT
 
 const router = createBrowserRouter([
   {
@@ -31,7 +28,10 @@ const router = createBrowserRouter([
   },
 ]);
 
-const App = () => <RouterProvider router={router} />;
+const App = () => (
+  <AuthProvider> {/* 👈 Le contexte englobe tout */}
+    <RouterProvider router={router} />
+  </AuthProvider>
+);
 
 export default App;
-
--- Frontend/src/containers/Roots.jsx
+++ Frontend/src/containers/Roots.jsx
@@ -1,17 +1,13 @@
-// RootLayout.jsx
 import Navigation from "../components/navigation/Navigation";
 import { Outlet } from "react-router-dom";
-import { AuthProvider } from "../components/context/AuthContext"; // 👈 import ici
 
 export default function RootLayout() {
     return (
-        <AuthProvider> {/* 👈 ENTOURE ICI */}
-            <>
-                <Navigation />
-                <main>
-                    <Outlet />
-                </main>
-            </>
-        </AuthProvider>
+        <>
+            <Navigation />
+            <main>
+                <Outlet />
+            </main>
+        </>
     );
 }

d6bfc901794b6486c187ccfe7606b279fba1d102
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-05-08 12:56:23 -0400

    languesà

--- Frontend/src/components/navigation/Navigation.jsx
+++ Frontend/src/components/navigation/Navigation.jsx
@@ -11,8 +11,9 @@ const Navigation = () => {
           <Link to="/connexion" className="soumission-lien">Soumissions</Link>
         </h1>
         <nav className="main-navigation_entete-nav">
-          <LanguageSwitcher />
+
           <NavLinks />
+          <LanguageSwitcher />
           {/* ✅ ICI, sous le NavLinks */}
           <div style={logoContainer}>
             <img src={require("./images/logomenu.png")} alt="Logo" style={logoStyle} />

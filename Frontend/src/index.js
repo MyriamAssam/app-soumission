df31d260fd8951f9e9f8f29ac4608578b6dceb8a
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-05-07 20:31:39 -0400

    langues

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,18 +4,16 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from "./components/context/AuthContext";
-
+import { I18nextProvider } from "react-i18next";
+import i18n from './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <I18nextProvider i18n={i18n}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </I18nextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

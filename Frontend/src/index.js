b1ad1c9e5c17f444ad76c4a00bc3ae10adefab3e
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-05-07 21:24:17 -0400

    langues

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,17 +4,14 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from "./components/context/AuthContext";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n"; // chemin vers ton instance i18n
+import './i18n'; // très important pour initialiser i18next
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </I18nextProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 

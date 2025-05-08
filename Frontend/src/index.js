44e3b00f1a487380f561258380458a69eb095144
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-05-07 20:41:14 -0400

    langues

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from "./components/context/AuthContext";
 import { I18nextProvider } from "react-i18next";
-import i18n from '../i18n';
+import i18n from "./i18n"; // chemin vers ton instance i18n
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,3 +17,5 @@ root.render(
     </I18nextProvider>
   </React.StrictMode>
 );
+
+reportWebVitals();

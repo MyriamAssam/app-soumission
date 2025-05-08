f0dbd3f71cd1cc34f73bea5d9b5590e8fe839cba
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-05-07 20:08:32 -0400

    langues

--- Frontend/src/containers/Register.jsx
+++ Frontend/src/containers/Register.jsx
@@ -1,10 +1,10 @@
 import "./Register.css";
 import Inscription from "../components/inscription/Inscription";
 import { useState } from "react";
-
+import { useTranslation } from "react-i18next";
 export default function Register() {
   const [typeCompte, setTypeCompte] = useState("Client");
-
+  const { t } = useTranslation();
   return (
     <div className="texte">
       <br />

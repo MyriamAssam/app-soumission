f34efda541fe3daf8526a9f9c7daf3edfe11f4ab
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-05-12 09:09:36 -0400

    date

--- Frontend/src/containers/AllSoumissions.jsx
+++ Frontend/src/containers/AllSoumissions.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../components/context/AuthContext";
 import moment from "moment";
+import "moment/locale/fr";
 import { useTranslation } from "react-i18next";
 
 const AllSoumissions = () => {

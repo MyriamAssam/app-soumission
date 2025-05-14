632325c31eaa28c61095b54a45bb80001b645df0
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-05-14 11:06:26 -0400

    css

--- Frontend/src/components/navigation/Navigation.jsx
+++ Frontend/src/components/navigation/Navigation.jsx
@@ -1,38 +1,39 @@
-import LanguageSwitcher from "../LanguageSwitcher";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import NavLinks from "./NavLinks";
+import LanguageSwitcher from "../LanguageSwitcher";
 import "./Navigation.css";
 
 const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <>
-      <div className="main-header-wrapper">
-        <header className="main-entete">
-          <div className="left-zone">
-            <div className="main-navigation_titre">
-              <Link to="/connexion" className="soumission-lien">Soumissions</Link>
-            </div>
-            <LanguageSwitcher />
+    <div className="main-header-wrapper">
+      <header className="main-entete">
+        <div className="left-zone">
+          <div className="main-navigation_titre">
+            <Link to="/connexion" className="soumission-lien">Soumissions</Link>
           </div>
+          <LanguageSwitcher />
+        </div>
 
-          <div className="main-navigation_entete-nav">
-            <ul className="navi-links"><NavLinks /></ul>
-          </div>
+        {/* ✅ Bouton hamburger */}
+        <div className="burger" onClick={() => setMenuOpen(prev => !prev)}>
+          ☰
+        </div>
 
-          <div className="logo-container">
-            <img src={require("./images/logomenu.png")} alt="Logo" className="logo-style" />
-          </div>
-        </header>
+        {/* ✅ Menu mobile toggle */}
+        <div className={`main-navigation_entete-nav ${menuOpen ? "open" : ""}`}>
+          <ul className="navi-links"><NavLinks /></ul>
+        </div>
 
-
-
-      </div>
-
-    </>
+        <div className="logo-container">
+          <img src={require("./images/logomenu.png")} alt="Logo" className="logo-style" />
+        </div>
+      </header>
+    </div>
   );
 };
 
-
-
 export default Navigation;
+

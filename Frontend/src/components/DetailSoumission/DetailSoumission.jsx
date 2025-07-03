5e27132126f95da6e8ce7dfdf9ca7ff42848ec16
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-07-03 13:11:10 -0400

    erreur add

--- Frontend/src/components/DetailSoumission/DetailSoumission.jsx
+++ Frontend/src/components/DetailSoumission/DetailSoumission.jsx
@@ -85,7 +85,8 @@ const DetailSoumission = () => {
                     id: Math.random().toString(36).substr(2, 9),
                     texte: note,
                     role: auth.role,
-                    auteur: auth.prenom
+                    auteur: auth.user.prenom
+
                 };
 
 
